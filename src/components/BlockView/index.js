53c79e1a43d565165b892a50ec07fb4538f94108
Ali Movahedi <ali_movahedi@aol.com>
2019-01-10 10:50:07 +0330

    feat: add Arrows to pages for navigating

--- src/components/BlockView/index.js
+++ src/components/BlockView/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import styles from './index.less';
 
 export default () => (
-  <p>
-    a
-  </p>
+  <div className={styles.container}>
+    <div className={styles.left}>
+      <FontAwesomeIcon icon={faArrowLeft} size="4x" />
+    </div>
+    <div className={styles.container}>
+      <p>
+        Hello fucking there!
+      </p>
+    </div>
+    <div className={styles.right}>
+      <FontAwesomeIcon icon={faArrowRight} size="4x" />
+    </div>
+  </div>
 );
