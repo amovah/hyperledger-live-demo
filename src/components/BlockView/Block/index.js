4120de56b46a3aecb0214aa00ca76d0a2c48a0fb
Ali Movahedi <ali_movahedi@aol.com>
2019-01-10 22:40:48 +0330

    feat: set max height var for content

--- src/components/BlockView/Block/index.js
+++ src/components/BlockView/Block/index.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import toggleShowing from 'Root/actions/blocks/toggleShowing';
 import styles from './index.less';
 
-export default (props) => {
-  return (
-    <div className={styles.container}>
+class Block extends Component {
+  container = React.createRef();
+
+  componentDidMount() {
+    const height = this.container.current.scrollHeight;
+    this.container.current.style.setProperty('--max-height', `${height}px`);
+  }
+
+  render() {
+    const {
+      data,
+    } = this.props;
+
+    return (
       <div
-        className={styles.header}
-        onClick={() => toggleShowing(props.data.blockhash)}
+        className={styles.container}
       >
-        <p>
-          {props.data.blocknum}
-        </p>
+        <div
+          className={styles.header}
+          onClick={() => toggleShowing(data.blockhash)}
+        >
+          <p>
+            {data.blocknum}
+          </p>
+        </div>
+        <div
+          className={styles.content}
+          ref={this.container}
+        >
+          a
+        </div>
       </div>
-      <div className={styles.content}>
-        a
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+export default Block;
