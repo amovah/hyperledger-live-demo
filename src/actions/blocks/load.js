315fc82de15bafbead4620685037bc3eec7dd704
Ali Movahedi <ali_movahedi@aol.com>
2019-01-10 21:47:31 +0330

    feat: add active status for blocks to toggle them

--- src/actions/blocks/load.js
+++ src/actions/blocks/load.js
@@ -5,6 +5,9 @@ import sample from './sample';
 export default async () => {
   store.dispatch({
     type: types.blocks.LOAD,
-    data: sample.rows.sort((a, b) => a.blocknum - b.blocknum),
+    data: sample.rows.sort((a, b) => a.blocknum - b.blocknum).map(i => ({
+      ...i,
+      active: false,
+    })),
   });
 };
