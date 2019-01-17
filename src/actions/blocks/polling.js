76e6083a089ea2768001101d0c4710b836bf216e
Ali Movahedi <ali_movahedi@aol.com>
2019-01-17 20:50:24 +0330

    fix: fix if there is no block in first fetching data

--- src/actions/blocks/polling.js
+++ src/actions/blocks/polling.js
@@ -5,7 +5,8 @@ import add from './add';
 const polling = async () => {
   const res = await fetch();
 
-  const lastNum = store.getState().blocks.slice(-1)[0].blocknum;
+  const lastBlock = store.getState().blocks.slice(-1)[0];
+  const lastNum = lastBlock ? lastBlock.blocknum : -1;
   add(res.data.rows.filter(i => i.blocknum > lastNum));
 
   setTimeout(polling, 10000);
